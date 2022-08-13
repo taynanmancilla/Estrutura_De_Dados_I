ad7112468be0cd94387acae7dd77a0f46c878f9e
taynanmancilla <taynan.estudo@gmail.com>
2022-08-13 00:18:55 -0400

    alocados

--- Aula/Selection_Sort/selection_main.c
+++ Aula/Selection_Sort/selection_main.c
@@ -2,25 +2,42 @@
 #include<stdlib.h>
 #include<time.h>
 #include"selection_sort.h"
-#define size 8
+
+void run(int *vetor, int size)
+{
+    srand(time(NULL));
+    for(int i=0;i<size; i++){
+        vetor[i] = rand()%99;
+    }
+    selection(vetor, size);
+    printSelection(vetor, size);
+}
 
 int main(){
 
-   // int data[] = {0, 2, 7, 0, 9, 3, 8, 0};
-    int vetor[size];
-    //vetor = (int*)calloc(size, sizeof(int));
+    int size = 10;
+    int *vetor;
 
-    srand(time(NULL));
-    for(int i=0;i<size; i++){
-        vetor[i] = rand()%9;
+    vetor = (int *)malloc(size*sizeof(int));
+    if(vetor == NULL){
+        printf("Erro de Alocacao!");
+        system("pause");
+        exit(1);
     }
+    printf("Vetor de %d posicoes:", size);
+    run(vetor, size);
 
-    for(int i=0; i<size; i++){
-        printf("|%d", vetor[i]);
-    }
-    printf("\n");
-    selection(vetor, size);
-    printSelection(vetor, size);
+    printf("\n\n");
+    size = 50;
+    vetor = (int *)realloc(vetor, size*sizeof(int));
+    printf("Vetor de %d posicoes:", size);
+    run(vetor, size);
+
+    printf("\n\n");
+    size = 100;
+    vetor = (int *)realloc(vetor, size*sizeof(int));
+    printf("Vetor de %d posicoes:", size);
+    run(vetor, size);
 
     free(vetor);
 
